4a5cbf972baa74fa4566f63168a5af062456836b
Boonchu <bigchoo@gmail.com>
2018-12-24 21:46:53 -0500

    output server ip

--- lab1/index.js
+++ lab1/index.js
@@ -1,13 +1,19 @@
+var os = require('os')
 var express = require('express')
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  var net = os.networkInterfaces();
+  var msg = 'server ip: ' + net.eth0[0].address + ' cidr: ' + net.eth0[0].cidr;
+  res.set('Content-Type', 'text/html');
+  res.send(msg);
+  res.status(200).end();
 });
 
 var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+  var server_info = {
+    ip: server.address().address,
+    port: server.address().port
+  };
+  console.log('App listening at http://%s:%s', server_info.ip, server_info.port);
 });
